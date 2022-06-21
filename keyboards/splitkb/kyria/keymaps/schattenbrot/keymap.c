f60a39ec2210d4b93a6500946ebff04abca6790f
Schattenbrot <aaron.machill@web.de>
2022-06-21 09:12:24 +0200

    Gaming layer right side keybinds

--- keyboards/splitkb/kyria/keymaps/schattenbrot/keymap.c
+++ keyboards/splitkb/kyria/keymaps/schattenbrot/keymap.c
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                       `----------------------------------'  `----------------------------------'
  */
     [_GAMING] = LAYOUT(
-     KC_TAB , KC_Y  , KC_Q  , KC_T  , KC_E   ,   KC_R ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     KC_ESC ,KC_LSFT, KC_A  , KC_W  , KC_D   ,   KC_F ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,  KC_SCLN , CTL_QUOT,
-     KC_N   ,KC_LCTL, KC_Z  , KC_S  , KC_C   ,   KC_V , KC_B   , KC_G  ,     FKEYS  , KC_RBRC, KC_N,   KC_M ,  KC_COMM ,   KC_DOT ,  KC_Z , KC_RSFT,
+     KC_Y , KC_TAB  , KC_Q  , KC_T  , KC_E   ,   KC_R ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P ,   KC_BSPC,
+     KC_ESC ,KC_LSFT, KC_A  , KC_W  , KC_D   ,   KC_F ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,  KC_SCLN, CTL_QUOT,
+     KC_N   ,KC_LCTL, KC_Z  , KC_S  , KC_C   ,   KC_V , KC_B   , KC_G  ,     FKEYS  , KC_RBRC, KC_N,   KC_M ,  KC_COMM ,KC_DOT, KC_Z ,   KC_RSFT,
                               KC_X  , KC_LALT, GAMENUM, KC_SPC , KC_M  ,     SYM    , KC_SPC , KC_RALT, KC_RGUI, KC_APP
     ),
 
